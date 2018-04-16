33ce0ad9fe4a509f858032e8dca5588a9c3d385a
damienbod <damien_bod@hotmail.com>
2018-04-16 22:52:18 +0200

    Adding AuthorizationService

---
+++ AuthorizationService/RulesAdmin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppAuthorizationService
+{
+    public static class RulesAdmin
+    {
+
+        private static List<string> adminUsers = new List<string>();
+
+        private static List<string> adminProviders = new List<string>();
+
+        public static bool IsAdmin(string username, string providerClaimValue)
+        {
+            if(adminUsers.Count == 0)
+            {
+                AddAllowedUsers();
+                AddAllowedProviders();
+            }
+
+            if (adminUsers.Contains(username) && adminProviders.Contains(providerClaimValue))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void AddAllowedUsers()
+        {
+            adminUsers.Add("SWISSANGULAR\\Damien");
+        }
+
+        private static void AddAllowedProviders()
+        {
+            adminProviders.Add("Windows");
+        }
+    }
+}
