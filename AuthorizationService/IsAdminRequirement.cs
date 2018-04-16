894c34ddf1b13b2aa3e840424b9fdbc9a764af01
damienbod <damien_bod@hotmail.com>
2018-04-16 22:53:37 +0200

    Add policies

---
+++ AuthorizationService/IsAdminRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+ 
+namespace AppAuthorizationService
+{
+    public class IsAdminRequirement : IAuthorizationRequirement
+    {
+    }
+}
---
+++ AuthorizationService/MyPolicies.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppAuthorizationService
+{
+    public static class MyPolicies
+    {
+        private static AuthorizationPolicy requireWindowsProviderPolicy;
+
+        public static AuthorizationPolicy GetRequireWindowsProviderPolicy()
+        {
+            if (requireWindowsProviderPolicy != null) return requireWindowsProviderPolicy;
+
+            requireWindowsProviderPolicy = new AuthorizationPolicyBuilder()
+                  .RequireClaim("http://schemas.microsoft.com/identity/claims/identityprovider", "Windows")
+                  .Build();
+
+            return requireWindowsProviderPolicy;
+        }
+    }
+}
